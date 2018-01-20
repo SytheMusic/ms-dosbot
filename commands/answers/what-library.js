da4ee0f823d5eaa420306bd9a3ba4f44e27aedce
acobeppp <34379730+acobeppp@users.noreply.github.com>
2018-01-20 16:29:39 +0100

    New command from Answer

---
+++ commands/answers/what-library.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
+class command extends commando.Command {
+  constructor() {
+    super(bot, {
+      name: 'what-library',
+      group: 'answers',
+      memberName: 'what-library',
+      description: 'What library?'
+    });
+  }
+
+async run(message, args) {
+  console.log('!what-library used.')
+  message.reply('Discord.js and Discord.js-commando.');
+  message.channel.send('Ok i 	__maybe__ switch to Eris because its also node.js and maybe simpler');
+  }
+}
+
+module.exports = command;
